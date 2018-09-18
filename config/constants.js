b23de535bc4c57db6dba81b54c09c9777154b7ed
SabarishJami <43333732+SabarishJami@users.noreply.github.com>
2018-09-18 19:46:20 +0530

    commit
    commit

--- config/constants.js
+++ config/constants.js
@@ -2,8 +2,8 @@ const config = {};
 
 config.constants = {
     Gigya: {
-        API_KEY: '3_DUUIeN2KHheyyUza_J2503FdmWdqUDoZCdt17sKYQ7fM45eaS8EYkFl7cmPliRwz',
-        SECRET_KEY: 'Nn0TiC9l0nlPPCnFl0Ci6GTsk2bXZeSwtsCLwf2bqg4'
+        API_KEY: '',
+        SECRET_KEY: ''
     },
     gigyaEndPoints: {
         accounts: {
